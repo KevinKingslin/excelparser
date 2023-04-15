7093b75a42c22ee87cf28323e153bd53c13b94b6
Kevin Kingslin <kevinkingslin@gmail.com>
2023-04-15 19:08:08 +0530

    Added sample excel sheet

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-3!_6sj5ez1mtunz&wnwktd-*3ed$h2k3z7p$a$*aa0r=v7^^l2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
